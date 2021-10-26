4c0a64c65762cd85155e31f7a570117006ab15ed
Cody Mercadante <cody.mercadante@wsu.edu>
2021-10-26 16:23:27 -0700

    deploying to heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+import path from 'path';
+import express from 'express';
+
+const PORT = process.env.PORT || 8081;
+const app = express();
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+app.listen(PORT, () => {
+    console.log(`Server listening at port ${PORT}.`);
+});
