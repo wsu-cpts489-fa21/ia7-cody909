0cd7e6a00c8fa09c648b7c22c4da65d2ad31511e
Cody Mercadante <cody.mercadante@wsu.edu>
2021-10-26 19:03:40 -0700

    added functionality for PopUpModal component

--- client/src/components/PopUpModal.js
+++ client/src/components/PopUpModal.js
@@ -1,8 +1,52 @@
+import { Modal, Button } from "react-bootstrap";
+import { useState } from "react";
 
 export default function PopUpModal(props) {
-    return (
-        <>
-        in development
-        </>
-    )
+  const [show, setShow] = useState(true);
+
+  const handleClose = () => {
+    setShow(false);
+    // on close execute the function associated with the last entry in the choices object
+    if (props.choices) {
+      const lastFunction =
+        props.choices[
+          Object.keys(props.choices)[Object.keys(props.choices).length - 1]
+        ];
+      try {
+        lastFunction();
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
+  return (
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{props.text || "no text provided"}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {props.choices &&
+            Object.entries(props.choices).map(([key, value]) => (
+              <>
+                <Button
+                  className="mb-3"
+                  onClick={() => {
+                    try {
+                      value();
+                    } catch (e) {
+                      console.log(e);
+                    }
+                  }}
+                >
+                  {key}
+                </Button>
+                <br />
+              </>
+            ))}
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 }
