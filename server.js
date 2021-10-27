a352f5ef482a7bf95870c97816472eb8d5ba846d
Cody Mercadante <cody.mercadante@wsu.edu>
2021-10-26 17:16:35 -0700

    deployment

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// import path from 'path';
-// import express from 'express';
-const path = require('path');
-const express = require('express');
-//const __dirname = process.cwd();
+import path from 'path';
+import express from 'express';
+// const path = require('path');
+// const express = require('express');
+const __dirname = process.cwd();
 
 const PORT = process.env.PORT || 8081;
 const app = express();
